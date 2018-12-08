5aac210d614ed8f95a8fc2a02aae305f438e5f4f
Jaela Wesley <jcwesley93@gmail.com>
2018-12-08 07:09:17 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
-def square_array(array)
-  # your code here
+[2, 3, 4, 5].each do |n|
+  text = "The square is: #{n ** 2}"
+  puts text
 end
+
+def square_array(array)
+ (array).each 
+end
+square_array([1, 2, 3])
